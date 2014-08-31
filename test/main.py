6f4dbdca4d1243f9845e6caeda11d7e5113c7fee
t4kash <t4kash@gmail.com>
2014-08-31 17:13:47 +0900

    Add test application

---
+++ test/main.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+import webapp2
+from google.appengine.ext import ndb
+
+
+class Model1(ndb.Model):
+    hoge = ndb.StringProperty()
+    fuga = ndb.StringProperty()
+    piyo = ndb.StringProperty()
+
+class Model2(ndb.Model):
+    hoge = ndb.StringProperty()
+    fuga = ndb.StringProperty()
+    piyo = ndb.StringProperty()
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+
+class MakeData(webapp2.RequestHandler):
+    def get(self):
+        hoge = ''
+        fuga = ''
+        piyo = ''
+        for i in range(10):
+            hoge += 'あいうえおかきくけこさしすせそ'
+            fuga += 'あいうえおかきくけこさしすせそ'
+            piyo += 'あいうえおかきくけこさしすせそ'
+
+        for i in range(30):
+            models = []
+            for j in range(10):
+                models.append(Model1(hoge=hoge, fuga=fuga, piyo=piyo))
+            ndb.put_multi(models)
+
+        for i in range(30):
+            models = []
+            for j in range(10):
+                models.append(Model2(hoge=hoge, fuga=fuga, piyo=piyo))
+            ndb.put_multi(models)
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('ok')
+
+
+app = webapp2.WSGIApplication(
+    [
+        ('/', MainPage),
+        ('/makedata', MakeData),
+    ], debug=True)
